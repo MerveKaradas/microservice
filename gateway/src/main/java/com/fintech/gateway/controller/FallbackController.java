7f7274efc4180fdedccdcc6d3b2cb6a4f326ec7e
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-10-22 00:44:48 +0300

    Gateway'e transaction-service dahil edildi

--- gateway/src/main/java/com/fintech/gateway/controller/FallbackController.java
+++ gateway/src/main/java/com/fintech/gateway/controller/FallbackController.java
@@ -6,27 +6,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 @RestController
+@RequestMapping("/fallback")
 public class FallbackController {
 
     //TODO : Gelişmiş fallback mekanizması oluşturulacak 
-    @RequestMapping("/fallback/auth")
+    @RequestMapping("/auth")
     public ResponseEntity<String> authServiceFallback() {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
               .body("Auth Service şuan kullanılamıyor. Lütfen daha sonra tekrar deneyiniz.");
     }
 
     //TODO : Gelişmiş fallback mekanizması oluşturulacak 
-    @RequestMapping("/fallback/user")
+    @RequestMapping("/user")
     public ResponseEntity<String> userServiceFallback() {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
               .body("User Service şuan kullanılamıyor. Lütfen daha sonra tekrar deneyiniz.");
     }
 
     //TODO : Gelişmiş fallback mekanizması oluşturulacak 
-    @RequestMapping("/fallback/account")
+    @RequestMapping("/account")
     public ResponseEntity<String> accountServiceFallback() {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
               .body("Account Service şuan kullanılamıyor. Lütfen daha sonra tekrar deneyiniz.");
+    }
+
+    //TODO : Gelişmiş fallback mekanizması oluşturulacak 
+    @RequestMapping("/transaction")
+    public ResponseEntity<String> transactionServiceFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+              .body("Transaction Service şuan kullanılamıyor. Lütfen daha sonra tekrar deneyiniz.");
     }
 
 
